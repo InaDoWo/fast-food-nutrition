bdbff97074a9c0dba723730b159322f11af19448
Ina <ina.wortmann@gmail.com>
2017-06-28 10:48:47 +0200

    design changes and responsive improvements

--- src/components/DietFilter.js
+++ src/components/DietFilter.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {ButtonGroup, Button, DropdownButton, MenuItem} from 'react-bootstrap'
+import {ButtonGroup, Button, DropdownButton, MenuItem, Grid} from 'react-bootstrap'
 import '../App.css';
 
 const DietFilter = ({onVegetarian, onVegan, onKcal, value, options, selectedVegetarian, selectedVegan}) => (
 
-    <div className="Diet-filter">
+    <Grid className="Diet-filter">
         <ButtonGroup justified>
-          {selectedVegetarian ? <Button onClick={e => onVegetarian(e, "vegetarian")} bsStyle="success" bsSize="large" href="#" active>Vegetarisch</Button>:<Button onClick={e => onVegetarian(e, "vegetarian")} bsSize="large" href="#">Vegetarisch</Button>}
-            {selectedVegan ? <Button onClick={e => onVegan(e, "vegan")} bsStyle="success" bsSize="large" href="#">Vegan</Button> : <Button onClick={e => onVegan(e, "vegan")} bsSize="large" href="#">Vegan</Button>}
-            <DropdownButton value={value} bsStyle="success" bsSize="large" title={"Bis " + value + " kcal"} id="bg-justified-dropdown">
-                {options.map(option => <MenuItem value={option} onClick={e => onKcal(option)} key={option} eventKey={option}>Bis {option} kcal</MenuItem>)
+          {selectedVegetarian ? <Button className="Filter-button" onClick={e => onVegetarian(e, "vegetarian")} bsStyle="success" bsSize="large" href="#" active>Vegetarisch</Button>:<Button onClick={e => onVegetarian(e, "vegetarian")} bsSize="large" href="#">Vegetarisch</Button>}
+            {selectedVegan ? <Button className="Filter-button" onClick={e => onVegan(e, "vegan")} bsStyle="success" bsSize="large" href="#">Vegan</Button> : <Button onClick={e => onVegan(e, "vegan")} bsSize="large" href="#">Vegan</Button>}
+            <DropdownButton className="Filter-button" value={value} bsStyle="success" bsSize="large" title={"Bis " + value + " kcal"} id="bg-justified-dropdown">
+                {options.map(option => <MenuItem value={option} onClick={e => onKcal(option)} key={option} eventKey={option}><p>Bis {option} kcal</p></MenuItem>)
 }
             </DropdownButton>
         </ButtonGroup>
-    </div>
+    </Grid>
 
 )
 DietFilter.propTypes = {
--- src/components/ProducerList.js
+++ src/components/ProducerList.js
@@ -10,29 +10,29 @@ const ProducerList = ({onProducer}) => (
                 <Col xs={0} md={2}></Col>
                 <Col xs={12} md={8}>
                     <Well>
-                        <Row>
-                            <Col className="Producer-icon" xs={5} md={5}>
-                                <button className="Producer-link" onClick={e => onProducer('mcdonalds')}><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/143px-McDonald%27s_Golden_Arches.svg.png" alt="McDonalds"/></button>
-                            </Col>
-                            <Col xs={7} md={7}>
-                                <button className="Producer-link Producer-title" onClick={e => onProducer('mcdonalds')}>
+                        <button className="Producer-link" onClick={e => onProducer('mcdonalds')}>
+                            <Row>
+                                <Col className="Producer-icon" xs={5} md={5}>
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/36/McDonald%27s_Golden_Arches.svg/143px-McDonald%27s_Golden_Arches.svg.png" alt="McDonalds"/>
+                                </Col>
+                                <Col xs={7} md={7}>
                                     <h3 className="Producer-name">McDonald&apos;s</h3>
-                                </button>
-                            </Col>
-                        </Row>
+                                </Col>
+                            </Row>
+                        </button>
                     </Well>
 
                     <Well>
-                        <Row>
-                            <Col className="Producer-icon" xs={5} md={5}>
-                                <button className="Producer-link" onClick={e => onProducer('burgerking')}><img src="https://upload.wikimedia.org/wikipedia/de/thumb/c/cb/Logo_Burger_King.svg/200px-Logo_Burger_King.svg.png" alt="Burger King"/></button>
-                            </Col>
-                            <Col xs={7} md={7}>
-                                <button className="Producer-link Producer-title" onClick={e => onProducer('burgerking')}>
+                        <button className="Producer-link" onClick={e => onProducer('burgerking')}>
+                            <Row>
+                                <Col className="Producer-icon" xs={5} md={5}>
+                                    <img src="https://upload.wikimedia.org/wikipedia/de/thumb/c/cb/Logo_Burger_King.svg/200px-Logo_Burger_King.svg.png" alt="Burger King"/>
+                                </Col>
+                                <Col xs={7} md={7}>
                                     <h3 className="Producer-name">Burger King</h3>
-                                </button>
-                            </Col>
-                        </Row>
+                                </Col>
+                            </Row>
+                        </button>
                     </Well>
                 </Col>
                 <Col xs={0} md={2}></Col>
